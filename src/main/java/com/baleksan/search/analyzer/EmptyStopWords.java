a7bf63e60fb482fdaedf8a190b067deb06a2c2fb
Boris Aleksandrovsky <baleksan@yammer-inc.com>
2011-05-09 09:50:13 -0700

    new stop words

--- src/main/java/com/baleksan/search/analyzer/EmptyStopWords.java
+++ src/main/java/com/baleksan/search/analyzer/EmptyStopWords.java
@@ -1,7 +1,25 @@
 package com.baleksan.search.analyzer;
 
+import java.util.Collections;
+import java.util.Set;
+
 /**
  * @author <a href="mailto:baleksan@yammer-inc.com" boris/>
  */
-public class EmptyStopWords {
+public class EmptyStopWords implements StopWords {
+    private static Set<String> stopWords;
+
+    static {
+        stopWords = Collections.emptySet();
+    }
+
+    @Override
+    public boolean isStopWord(String word) {
+        return stopWords.contains(word.toLowerCase());
+    }
+
+    @Override
+    public Set<String> getStopWords() {
+        return stopWords;
+    }
 }
--- src/main/java/com/baleksan/search/analyzer/StandardAnalyzerStopWords.java
+++ src/main/java/com/baleksan/search/analyzer/StandardAnalyzerStopWords.java
@@ -1,7 +1,30 @@
 package com.baleksan.search.analyzer;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author <a href="mailto:baleksan@yammer-inc.com" boris/>
  */
-public class StandardAnalyzerStopWords {
+public class StandardAnalyzerStopWords  implements StopWords {
+    private static Set<String> stopWords;
+
+    static {
+        stopWords = new HashSet<String>();
+        for (Object word : StandardAnalyzer.STOP_WORDS_SET) {
+            stopWords.add((String) word);
+        }
+    }
+
+    @Override
+    public boolean isStopWord(String word) {
+        return stopWords.contains(word.toLowerCase());
+    }
+
+    @Override
+    public Set<String> getStopWords() {
+        return stopWords;
+    }
 }
